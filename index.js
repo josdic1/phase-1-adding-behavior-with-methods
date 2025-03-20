c8a239c38b55abeeafcb2c0920705f9ba9e63694
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-03-20 12:31:59 -0400

    adding behavior with mehods

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+class Cat {
+    constructor(name, sex) {
+       this.name = name;
+       this.sex = sex;
+       this.speak = function () {
+          return 'Sasha says meow!'
+       }
+    }
+ }
+ 
+ 
+ class Dog {
+    constructor(name, sex) {
+       this.name = name;
+       this.sex = sex;
+       this.speak = function () {
+          return 'Rufio says woof!'
+       }
+    }
+ }
+ 
+ class Bird {
+    constructor(name, sex) {
+       this.name = name;
+       this.sex = sex;
+       this.speak = function () {
+          if (sex === 'male') {
+             return 'It\'s me! Pablo, the parrot!'
+          } else {
+             return "Mable says squawk!"
+          }
+       }
+    }
+ }
+ 
+ const bird = new Bird("Pablo", "male")
+ const bird2 = new Bird("Mable", "female")
+ 
+ 
